80465d4656d06163457d55067f4080329db11c02
Adnane Hmunna <adn.hmunna@gmail.com>
2020-01-24 21:52:22 +0100

    1

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import autoComplete from 'autocomplete.js';
 import '../App.css';
-function Form()
+class Form extends React.Component 
 {
-    return(
-        <div>
-            Hello
-        </div>
-    );
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            value : ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.queryAdress = this.queryAdress.bind(this);
+        this.autoComplete = this.autoComplete.bind(this);
+    }
+
+    //Input Form Handler Function
+    async handleChange(event)
+    {
+        await this.setState({value : event.target.value});
+        if( this.state.value !== '') this.queryAdress();        
+    }
+
+    queryAdress()
+    {
+        return fetch(`https://api-adresse.data.gouv.fr/search/?q=${this.state.value}&limit=100&autocomplete=1`)
+        .then(res => res.json())
+        .then((result) => result.features)
+    }
+
+    autoComplete()
+    {
+
+    }
+
+    //Submit Form Handler Function
+    handleSubmit(event)
+    {
+        event.prevenDefault();
+    }
+
+    render()
+    {
+        return(
+            <div className="container">
+                <input  type="text" value={this.state.value} onChange={this.handleChange}/>
+                <input type="submit" value="Search" />
+            </div>
+        );
+    }
+   
 }
 export default Form;
