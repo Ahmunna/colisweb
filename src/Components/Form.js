6f094fb266a93c321740a2c2036de0a50494e127
Adnane Hmunna <adn.hmunna@gmail.com>
2020-02-03 20:49:37 +0100

    refractor code

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,146 +1,118 @@
-import React from 'react';
+import React,{useState} from 'react';
 import MapContainer from './MapContainer';
 import '../App.css';
-class Form extends React.Component 
+const Form = () =>
 {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            userInput : '',
-            activeAdress : 0,
-            adressesList : [],
-            showAdresses : false,
-            adressCoordinates : [],
-            activeAdressLongitude : 0,
-            activeAdressLatitude : 0
-
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.queryAdress = this.queryAdress.bind(this);
-        this.handleKeysEvents = this.handleKeysEvents.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
+    //define states
+    const [userInput,setUserInput] = useState('');
+    const [activeAdress,setActiveAdress] = useState(0);
+    const [adressesList,setAdressesList] = useState([]);
+    const [showAdresses,setShowAdresses] = useState(false);
+    const [adressCoordinates,setAdressCoordinates] = useState([]);
+    const [activeAdressLongitude,setActiveAdressLongitude] = useState(0);
+    const [activeAdressLatitude,setActiveAdressLatitude] = useState(0);
+    
 
     //Input Form Handler Function
-    async handleChange(event)
+    const handleChange = async (event) =>
     {
-        await this.setState({
-            userInput : event.currentTarget.value,
-            showAdresses : true
-        });
-        if( this.state.userInput !== '') this.queryAdress();        
+        await Promise.all([setUserInput(event.currentTarget.value),setShowAdresses(true)])
+        if( userInput !== '') queryAdress();        
     }
     //this function uses Fetch to get the result from the GEO api and updates the component's state
-    queryAdress()
+    const queryAdress = () =>
     {
-        return fetch(`https://api-adresse.data.gouv.fr/search/?q=${this.state.userInput}&limit=100&autocomplete=1`)
+        return fetch(`https://api-adresse.data.gouv.fr/search/?q=${userInput}&limit=100&autocomplete=1`)
         .then(res => res.json())
-        .then((result) => this.setState({adressesList : result.features}))
+        .then((result) => setAdressesList(result.features))
     }
 
     
     //Submit Form Handler Function
-    handleSubmit(event)
+    const handleSubmit = (event) =>
     {
         event.preventDefault();
-        const {adressCoordinates, activeAdressLongitude,activeAdressLatitude } = this.state;
-        this.setState({
-            adressCoordinates : [...adressCoordinates, {longitude : activeAdressLongitude, latitude : activeAdressLatitude}]
-        })
+        setAdressCoordinates([...adressCoordinates,{longitude : activeAdressLongitude, latitude : activeAdressLatitude}])
     }
 
     //handle key events
-    async handleKeysEvents(event)
+    const handleKeysEvents = (event) =>
     {
-        const {activeAdress, adressesList} = this.state;
         //if the user clicks enter
         if(event.keyCode === 13)
         {
-            this.setState({
-                activeAdress : 0 ,
-                showAdresses : false, 
-                userInput : adressesList[activeAdress].properties.label,
-                activeAdressLongitude : adressesList[activeAdress].geometry.coordinates[0],
-                activeAdressLatitude : adressesList[activeAdress].geometry.coordinates[1]
-            })
-            
+            setActiveAdress(0);
+            setShowAdresses(false);
+            setUserInput(adressesList[activeAdress].properties.label);
+            setActiveAdressLongitude(adressesList[activeAdress].geometry.coordinates[0]);
+            setActiveAdressLatitude( adressesList[activeAdress].geometry.coordinates[1]);
         }
         //if the user clicks up arrow 
         else if(event.keyCode === 38)
         {
             if(activeAdress === 0 ) return;
-            this.setState({activeAdress : activeAdress -1})
+            setActiveAdress(activeAdress -1);
         }
         //if the user click down arrow
         else if(event.keyCode === 40)
         {
             if(activeAdress -1 === adressesList.length) return;
-
-            this.setState({activeAdress : activeAdress + 1})
-            
+            setActiveAdress(activeAdress  + 1);
         }
     };
 
     //handle Click
-    handleClick(event)
+    const handleClick = (event) =>
     {
-        const {adressesList, activeAdress} = this.state;
-        this.setState({
-            activeAdress : 0,
-            showAdresses : false,
-            userInput : event.currentTarget.innerText,
-            activeAdressLongitude : adressesList[activeAdress].geometry.coordinates[0],
-            activeAdressLatitude : adressesList[activeAdress].geometry.coordinates[1]
-        })
+        setActiveAdress(0);
+        setShowAdresses(false);
+        setUserInput(event.currentTarget.innerText);
+        setActiveAdressLongitude(adressesList[activeAdress].geometry.coordinates[0]);
+        setActiveAdressLatitude(adressesList[activeAdress].geometry.coordinates[1]);
     }
     
-    render()
+    let list;
+    if(userInput !== '' && showAdresses)
     {
-        let list;
-        if(this.state.userInput !== '' && this.state.showAdresses)
+        if(adressesList.length)
         {
-            if(this.state.adressesList.length)
-            {
-                list = (
-                    <ul className="suggestions">
-                        {this.state.adressesList.map((adress,index) => {
-                            
-                            let className;
-                            if(index === this.state.activeAdress)
-                            {
-                                className = "suggestion-active";
-                            }
-                            return(
-                                <li className={className} key={adress.properties.id} onClick={this.handleClick}>
-                                    {adress.properties.label}
-                                </li>
-                            );
-                        })}
-                    </ul>
-                );
-            }
-            else
-            {
-                list = (
-                    <div className="no-suggestions">
-                        <em> No adresses Found !</em>
-                    </div>
-                );
-            }
+            list = (
+                <ul className="suggestions">
+                    {adressesList.map((adress,index) => {
+                        
+                        let className;
+                        if(index === activeAdress)
+                        {
+                            className = "suggestion-active";
+                        }
+                        return(
+                            <li className={className} key={adress.properties.id} onClick={handleClick}>
+                                {adress.properties.label}
+                            </li>
+                        );
+                    })}
+                </ul>
+            );
         }
-        return(
-            <div className="container">
-                <div>
-                    <input id="autoComplete" type="text" value={this.state.userInput} onChange={this.handleChange}  onKeyDown={this.handleKeysEvents}/>
-                    {list}
+        else
+        {
+            list = (
+                <div className="no-suggestions">
+                    <em> No adresses Found !</em>
                 </div>
-                <input type="submit" value="search" onClick={this.handleSubmit}/>
-                {this.state.adressCoordinates.length > 0 && <MapContainer coordinates={this.state.adressCoordinates} />}
-            </div>
-        );
+            );
+        }
     }
+    return(
+        <div className="container">
+            <div>
+                <input id="autoComplete" type="text" value={userInput} onChange={handleChange}  onKeyDown={handleKeysEvents}/>
+                {list}
+            </div>
+            <input type="submit" value="search" onClick={handleSubmit}/>
+            {adressCoordinates.length > 0 && <MapContainer coordinates={adressCoordinates} />}
+        </div>
+    );
    
 }
 export default Form;
--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -8,40 +8,32 @@ import '../App.css';
 This component is responsable for displaying the map using Google Maps api
 
 */
-class MapContainer extends React.Component
+const MapContainer = props =>
 {
     //this function displays all the markers from the props to the map
-    displayMarkers = () => {
-      return this.props.coordinates.map((coordinate, index) => {
+    const displayMarkers = () => {
+      return props.coordinates.map((coordinate, index) => {
         return <Marker key={index} id={index} position={{
          lat: coordinate.latitude,
          lng: coordinate.longitude
        }}
-       onClick={() => console.log("You clicked me!")} />
+       />
       })
     }
 
-    shouldComponentUpdate(nextProps,nextState)
-    {
-      if(this.props.coordinates.length === nextProps.coordinates.length) return false;
-      return true;
-    }
+    /* France position as initial center for the map*/
+      return(
+            <Map
+            google={props.google}
+            zoom={5}
+            style={mapStyle}
+            initialCenter={{ lat: 48.864716, lng: 2.349014}}
+          >
+          {displayMarkers()}
+          </Map>  
+        
+      );
 
-    render()
-    {
-       /* France position as initial center for the map*/
-        return(
-              <Map
-              google={this.props.google}
-              zoom={5}
-              style={mapStyle}
-              initialCenter={{ lat: 48.864716, lng: 2.349014}}
-            >
-            {this.displayMarkers()}
-            </Map>  
-          
-        );
-    }
 }
 
 const mapStyle = {
@@ -50,7 +42,9 @@ const mapStyle = {
   height : '50%',
 }
 
+const googleWrapper = GoogleApiWrapper({ apiKey: config.apiKey })(MapContainer);
 
-export default GoogleApiWrapper({
-    apiKey: config.apiKey
-  })(MapContainer);
+/*
+React.memo was introduced in React 16.6 To replace the lifecyle method shouldComponentUpdate
+*/
+export default React.memo(googleWrapper);
